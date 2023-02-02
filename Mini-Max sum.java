7100349534ee76488972a19b751562581617c9e4
venkat <82475753+siddu369@users.noreply.github.com>
2023-02-02 21:49:43 +0530

    Create Mini-Max sum.java

---
+++ Mini-Max sum.java
@@ -0,0 +1,12 @@
+class Result {
+  public static void miniMaxSum(List<Integer> arr) {
+    arr = arr.stream().sorted(Comparator.naturalOrder()).collect(Collectors.toList());
+    long sum = 0;
+    for (int i = 0; i < arr.size(); i++){
+        sum += arr.get(i);
+    }
+    long max = sum - arr.get(arr.size() - 1);
+    long min = sum - arr.get(0);
+    System.out.printf(max + " " + min);
+  }
+}
